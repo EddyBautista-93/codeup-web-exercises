4eaddbfbec05fc727528677221c0a5791d25ff45
Eddy Bautista <bautista.j.eddy@gmail.com>
2019-08-05 15:10:20 -0500

    lec notes

---
+++ JS/js-with-html-lecture.js
@@ -0,0 +1,9 @@
+"use strict";
+// alert("Message goes here");
+// console.log(confirm ("You are awesome"));
+
+
+// confirms returns a boolean va;ie
+
+// prompt return the value entered by the use;
+
