928789100eacb50f7e9c488c53ddb120b18184c3
Eddy Bautista <bautista.j.eddy@gmail.com>
2019-08-12 16:30:24 -0500

    loop exercise

---
+++ JS/for_loops.js
@@ -0,0 +1,35 @@
+"use strict";
+//
+// function showMultiplication(num) {
+//
+//     for(var i = 1; i <= 10; i++){
+//         console.log(num +"x" + i + "=" + (i * num));
+//     }
+//
+// }
+//
+//
+// showMultiplication(9)
+
+// for(var i = 1; i<=10 ;i++){
+// var num = Math.floor(Math.random() * 200);
+//     if(num % 2 === 0){
+//         console.log(num + " is Even");
+//     }else{
+//         console.log(num + " is Odd");
+//     }
+// }
+//
+
+
+// var num = "";
+// var size = 9;
+// for(var i=1; i<=size; i++)
+// {
+//     num = num + i;
+//     console.log(num);
+// }
+
+// for(var i = 100; i >= 0 ;i -= 5){
+//     console.log(i)
+// }
