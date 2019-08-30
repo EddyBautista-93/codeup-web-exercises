8afbfd0ffdf546d4a65cf6b29e539f0d4749725a
Eddy Bautista <bautista.j.eddy@gmail.com>
2019-08-30 14:37:42 -0500

    still needs some work

--- JS/keys.js
+++ JS/keys.js
@@ -1,1 +1,3 @@
-var mapboxToken = pk.eyJ1IjoiZWRkeWJhdXRpc3RhIiwiYSI6ImNqemloYjlrczBoNnEzb254bm9pNTBtc2IifQ.H2KMAvGQPNdIejNDhnz1Iw
+var  mapboxToken = "pk.eyJ1IjoiZWRkeWJhdXRpc3RhIiwiYSI6ImNqemloYjlrczBoNnEzb254bm9pNTBtc2IifQ.H2KMAvGQPNdIejNDhnz1Iw";
+
+var darkSkyKey = '04de43176f0ddf4f2fd77f754be521a1';
