0bc8644ea4cca6149e28d5b03d6673be91bf6b31
Eddy Bautista <bautista.j.eddy@gmail.com>
2019-08-30 16:10:53 -0500

    added icons

--- JS/keys.js
+++ JS/keys.js
@@ -1,3 +1,3 @@
-var  mapboxToken = "pk.eyJ1IjoiZWRkeWJhdXRpc3RhIiwiYSI6ImNqemloYjlrczBoNnEzb254bm9pNTBtc2IifQ.H2KMAvGQPNdIejNDhnz1Iw";
+var  mapboxToken = "pk.eyJ1IjoiZWRkeWJhdXRpc3RhIiwiYSI6ImNqemloYWY1czE2dzYzY253M3VydHo4ODkifQ.-V9O-Cx6Aw9zJDBrqDcSXg";
 
 var darkSkyKey = '04de43176f0ddf4f2fd77f754be521a1';
