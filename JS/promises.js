c629dabee823e498b37cfdc4f924933ec45f71bd
Eddy Bautista <bautista.j.eddy@gmail.com>
2019-09-10 15:38:51 -0500

    completed work

--- JS/promises.js
+++ JS/promises.js
@@ -1,15 +1,20 @@
 
 
 
-const wait = (num) => {
-        return new Promise((resolve) => {
-            setTimeout( () => {
-            resolve();
-            },num);
-        })
+// const wait = (num) => {
+//         return new Promise((resolve) => {
+//             setTimeout( () => {
+//             resolve();
+//             },num);
+//         })
+//
+// };
+//
+//
+// wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+// wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
-};
 
+const gitKey = '0edcbf0be7661c26dc5e085de056470dec1c0ccb';
 
-wait(1000).then(() => console.log('You\'ll see this after 1 second'));
-wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+// fetch(url, {headers: {'Authorization': 'gitKey'}});
