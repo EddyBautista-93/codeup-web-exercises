9f1490c7794b965343ef5b60a21f2777d4dfb607
Eddy Bautista <bautista.j.eddy@gmail.com>
2019-12-26 22:56:34 -0600

    so much extra code but its times to clean it up and make it look amazing !

--- JS/while.js
+++ JS/while.js
@@ -1,31 +1,31 @@
 "use strict";
 
-// var i = 2;
-//
-// while (i < 70000) {
-//     console.log(i);
-//     i = i * 2;
-// }
+// increment loop for excercise one in a arrow function
+const loopBy2 = () => {
+    let i = 2;
+    while (i < 70000){
+        console.log(i);
+        i = i * 2;
+    }
+};
 
+console.log(loopBy2());
 
-// This is how you get a random number between 50 and 100
-// var allCones = Math.floor(Math.random() * 50) + 50;
-// This expression will generate a random number between 1 and 5
-//
-// console.log("lets get to work i got " + allCones + " CONES");
-//
-//
-// do {
-// var conesSold = Math.floor(Math.random() * 5) + 1;
-// if(conesSold > allCones){
-//     console.log("Sorry my dude i dont have enough")
-// } else {
-//     allCones = allCones - conesSold;
-//     console.log("Here you are " +  + conesSold + " cones, I have " + allCones +" to sell")
-//
-// }
-//
-//
-// }while (allCones > 0);
-//
-// console.log("yah, I Sold all the cones");
+//Ice cream man sim made for class exercise
+const iceCreamSimulator = () => {
+let allCones = Math.floor(Math.random() * 50) + 50;
+    console.log(`I currently have ${allCones} cones left`);
+    do{
+        let conesSold = Math.floor(Math.random() * 5) + 1;
+        if(conesSold > allCones){
+            console.log(`Sorry I only have ${allCones}, I dont have enough`);
+        } else{
+             allCones = allCones - conesSold;
+            console.log(`I sold ${conesSold} cones, i have ${allCones} left`);
+        }
+    } while (allCones > 0);
+    console.log("I am all out of cones")
+};
+
+console.log(iceCreamSimulator());
+
